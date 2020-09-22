f282fcf15d8db4a2bf7af1ecfb859c61ee385e36
FinalTroy <ondrejicka.tadeas@gmail.com>
2020-09-22 10:00:04 +0200

    uprava stavu

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import { Layout, Steps } from "antd"
-import React, { FC, useContext, useEffect, useState } from "react"
+import React, { FC, useContext, useEffect, useMemo, useState } from "react"
 import { FormContext } from "../context/Context"
 import bloky from "../data/bloky"
 import _ from 'lodash'
@@ -8,9 +8,24 @@ const { Sider, Content } = Layout
 const { Step } = Steps
 export const Main: FC = () => {
     const { data, setData } = useContext(FormContext); // Data z Contextu
-    const [progress, setProgress] = useState<number>(0); // Hodnota progess baru
     const [activeBlok, setActiveBlok] = useState<string | null>(null); // nastaveni aktivniho bloku
-
+    // Ukladani aktivniho nazvu bloku
+    useEffect(() => {        
+        bloky.map((blok, index) => {
+            if (index === data.step) return setActiveBlok(blok.key.toString())
+        })
+    },[data.step])
+    // Memoizovani progress baru na zmenu dat a bloku
+    const progress = useMemo(() => {
+        const fields = _.get(data, `form.${activeBlok}`, {})
+        const fieldLength = Object.keys(fields).length
+        let valid: number = 0
+        for (const [key, value] of Object.entries(fields)) {
+            if (_.get(value, 'isValid') === true) valid++
+        }
+        const num = (valid / fieldLength) * 100 // procentualni hodnota vyplnenych fieldu
+        return !num ? 0 : num
+    }, [data, activeBlok])
     return (<>
         <Layout>
             <Sider style={{
@@ -33,19 +48,8 @@ export const Main: FC = () => {
         <Layout className="site-layout" style={{ marginLeft: 200 }}>
             <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
                 {bloky.map((blok, index) => {
-                    if (index === data.step) { // provizorni podminka pro zobrazeni bloku
-                        setActiveBlok(blok.key) // toto dela infinite loop
-                        const fields = _.get(data, `form.${activeBlok}`, {})
-                        const fieldLength = Object.keys(fields).length
-                        let valid: number = 0
-                        for (const [key, value] of Object.entries(fields)) {
-                            if (_.get(value, 'isValid') === true) valid++
-                        }
-                        const num = (valid / fieldLength) * 100 // procentualni hodnota vyplnenych fieldu
-                        setProgress(num) // toto dela infinite loop
-                        return <blok.component />
-                    }
-                    return <></>
+                    if (index === data.step) return <blok.component />
+                        return <></>
                 })}
             </Content>
         </Layout>
